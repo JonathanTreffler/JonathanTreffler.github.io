0491f9448bb7ebe554ac2a02b5e98bd8290b7d75
Jonathan Treffler <mail@jonathan-treffler.de>
2020-08-09 21:12:33 +0200

    Update Footer.vue

--- jonathan-treffler.de/components/Footer.vue
+++ jonathan-treffler.de/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="footer">
 		<a href="https://github.com/JonathanTreffler"><img align="left" alt="Jonathan Treffler | Github" width="22px" src="/projects/github.webp" /></a>
-		<a href="https://twitter.com/TessyPowder"><img align="left" alt="Jonathan Treffler | Twitter" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/twitter.svg" /></a>
+		<a href="https://twitter.com/treffler_j"><img align="left" alt="Jonathan Treffler | Twitter" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/twitter.svg" /></a>
 		<a href="https://www.npmjs.com/~jonathan_treffler"><img align="left" alt="Jonathan Treffler | NPM" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/npm.svg" /></a>
 		<a href="https://dev.to/jonathantreffler"><img align="left" alt="Jonathan Treffler | DEV" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/dev-dot-to.svg" /></a>
 		<a href="https://www.youtube.com/channel/UCeNkM_i1i9_Ver9njtxLAqw"><img align="left" alt="Jonathan Treffler | YouTube" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/youtube.svg" /></a>
