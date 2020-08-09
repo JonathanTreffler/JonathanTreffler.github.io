edca46ba6497fe77168943f833c674d318c143d2
Jonathan Treffler <mail@jonathan-treffler.de>
2020-08-09 20:37:42 +0200

    Update index.vue

--- jonathan-treffler.de/pages/index.vue
+++ jonathan-treffler.de/pages/index.vue
@@ -2,17 +2,20 @@
 	<div id="container">
 		<Header />
 		<Body />
+		<Footer />
 	</div>
 </template>
 
 <script>
 import Header from '~/components/Header.vue';
 import Body from '~/components/Body.vue';
+import Footer from '~/components/Footer.vue';
 
 export default {
 	components: {
 		Header,
 		Body,
+		Footer,
 	},
 };
 </script>
