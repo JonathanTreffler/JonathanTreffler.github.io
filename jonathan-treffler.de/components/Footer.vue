9e204ac0e39e1bc720a02ca4c4978558dc06353e
Jonathan Treffler <mail@jonathan-treffler.de>
2020-08-09 20:40:53 +0200

    Update Footer.vue

--- jonathan-treffler.de/components/Footer.vue
+++ jonathan-treffler.de/components/Footer.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>
-    <a href="https://jonathan-treffler.de"><img align="left" alt="jonathan-treffler.de" width="22px" src="https://raw.githubusercontent.com/iconic/open-iconic/master/svg/globe.svg" /></a>
-    <a href="https://twitter.com/TessyPowder"><img align="left" alt="Jonathan Treffler | Twitter" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/twitter.svg" /></a>
-    <a href="https://www.npmjs.com/~jonathan_treffler"><img align="left" alt="Jonathan Treffler | NPM" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/npm.svg" /></a>
-    <a href="https://dev.to/jonathantreffler"><img align="left" alt="Jonathan Treffler | DEV" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/dev-dot-to.svg" /></a>
-    <a href="https://www.youtube.com/channel/UCeNkM_i1i9_Ver9njtxLAqw"><img align="left" alt="Jonathan Treffler | YouTube" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/youtube.svg" /></a>
-  </div>
+	<div class="footer">
+		<a href="https://jonathan-treffler.de"><img align="left" alt="jonathan-treffler.de" width="22px" src="https://raw.githubusercontent.com/iconic/open-iconic/master/svg/globe.svg" /></a>
+		<a href="https://twitter.com/TessyPowder"><img align="left" alt="Jonathan Treffler | Twitter" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/twitter.svg" /></a>
+		<a href="https://www.npmjs.com/~jonathan_treffler"><img align="left" alt="Jonathan Treffler | NPM" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/npm.svg" /></a>
+		<a href="https://dev.to/jonathantreffler"><img align="left" alt="Jonathan Treffler | DEV" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/dev-dot-to.svg" /></a>
+		<a href="https://www.youtube.com/channel/UCeNkM_i1i9_Ver9njtxLAqw"><img align="left" alt="Jonathan Treffler | YouTube" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/youtube.svg" /></a>
+	</div>
 </template>
+<style>
+.footer {
+	display: flex;
+	width: 100%;
+	justify-content: center;
+}
+</style>
