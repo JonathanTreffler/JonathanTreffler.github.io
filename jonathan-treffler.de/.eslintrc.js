922cfab8fadc598e71359b05a1b95ac27de5c795
Jonathan Treffler <mail@jonathan-treffler.de>
2020-09-02 20:49:54 +0200

    Update .eslintrc.js

--- jonathan-treffler.de/.eslintrc.js
+++ jonathan-treffler.de/.eslintrc.js
@@ -1,34 +1,7 @@
 module.exports = {
-	root: true,
-	env: {
-		browser: true,
-		node: true,
-	},
-	parserOptions: {
-		parser: 'babel-eslint',
-	},
 	extends: [
 		'@nuxtjs',
 		'plugin:nuxt/recommended',
+		'jonathantreffler',
 	],
-	plugins: [
-	],
-	// add your custom rules here
-	rules: {
-		'comma-dangle': [ 'error', {
-			arrays: 'always',
-			objects: 'always',
-			imports: 'never',
-			exports: 'never',
-			functions: 'never',
-		}, ],
-		semi: [ 'error', 'always', ],
-		indent: [ 'error', 'tab', ],
-		'vue/html-indent': [ 'error', 'tab', ],
-		'no-tabs': 0,
-		'array-bracket-spacing': [ 'error', 'always', ],
-		'comma-style': [ 'error', 'last', ],
-		'comma-spacing': [ 'error', { before: false, after: true, }, ],
-		'space-before-function-paren': [ 'error', 'never', ],
-	},
 };
