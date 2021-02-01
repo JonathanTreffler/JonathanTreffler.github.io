a8c434e55a7067bc373b5473342cd06206d78728
Jonathan Treffler <mail@jonathan-treffler.de>
2021-02-01 00:27:50 +0000

    added github names to projects

--- jonathan-treffler.de/components/Projects.vue
+++ jonathan-treffler.de/components/Projects.vue
@@ -5,11 +5,13 @@
 				name="Notes Core"
 				icon="/projects/Notes.webp"
 				href="https://github.com/fractava/notes-core"
+				github-path="fractava/notes-core"
 			/>
 			<Project
 				name="Notes Desktop"
 				icon="/projects/Notes.webp"
 				href="https://github.com/fractava/notes-desktop"
+				github-path=""
 			/>
 			<Project
 				name="fractava.com"
@@ -27,16 +29,19 @@
 				name="School Squirrel"
 				icon="/projects/schoolSquirrel.webp"
 				href="https://github.com/SchoolSquirrel/SchoolSquirrel"
+				github-path="SchoolSquirrel/SchoolSquirrel"
 			/>
 			<Project
 				name="School Squirrel Fcm Relay"
 				icon="/projects/schoolSquirrel.webp"
 				href="https://github.com/SchoolSquirrel/SquirrelFcmRelay"
+				github-path="SchoolSquirrel/SquirrelFcmRelay"
 			/>
 			<Project
 				name="School Squirrel Chat"
 				icon="/projects/schoolSquirrel.webp"
 				href="https://github.com/SchoolSquirrel/SquirrelChatUI"
+				github-path="SchoolSquirrel/SquirrelChatUI"
 			/>
 		</ProjectsContainer>
 		<ProjectsContainer title="Jugend Forscht Quantum 2019">
@@ -44,26 +49,31 @@
 				name="Jufo: Quantum Computing"
 				icon="/projects/quantum.webp"
 				href="https://github.com/Quantum-Computing-Jufo-2019/JuFo-2019-Quantum-Computing"
+				github-path="Quantum-Computing-Jufo-2019/JuFo-2019-Quantum-Computing"
 			/>
 			<Project
 				name="Quantum Computer GUI"
 				icon="/projects/quantum.webp"
 				href="https://github.com/Quantum-Computing-Jufo-2019/quantum-computer-GUI"
+				github-path="Quantum-Computing-Jufo-2019/quantum-computer-GUI"
 			/>
 			<Project
 				name="Dwave Quantum Computer Animation"
 				icon="/projects/quantum.webp"
 				href="https://github.com/Quantum-Computing-Jufo-2019/dwave-quantum-computer-3d-animation"
+				github-path="Quantum-Computing-Jufo-2019/dwave-quantum-computer-3d-animation"
 			/>
 			<Project
 				name="domspatzen-quantum.de"
 				icon="/projects/quantum.webp"
 				href="https://github.com/Quantum-Computing-Jufo-2019/quantum-computing-jufo-2019.github.io"
+				github-path="Quantum-Computing-Jufo-2019/quantum-computing-jufo-2019.github.io"
 			/>
 			<Project
 				name="n Queens Chess App"
 				icon="https://raw.githubusercontent.com/Quantum-Computing-Jufo-2019/n-Queens-Android/master/src/nDamen_Schachfeld_Android/icon-144.png"
 				href="https://github.com/Quantum-Computing-Jufo-2019/n-Queens-Android"
+				github-path="Quantum-Computing-Jufo-2019/n-Queens-Android"
 			/>
 		</ProjectsContainer>
 		<ProjectsContainer title="Jugend Forscht Ameisen 2019">
@@ -71,16 +81,19 @@
 				name="Jufo: Ameisen 2019"
 				icon="/projects/ants.webp"
 				href="https://github.com/jufo-ameisen-2019/Jufo-Ameisen-2019"
+				github-path="jufo-ameisen-2019/Jufo-Ameisen-2019"
 			/>
 			<Project
 				name="Ameisen Simulator"
 				icon="/projects/ants.webp"
 				href="https://github.com/jufo-ameisen-2019/ameisen-simulator"
+				github-path="jufo-ameisen-2019/ameisen-simulator"
 			/>
 			<Project
 				name="Ameisen Deep Space Ars Electronica"
 				icon="/projects/ants.webp"
 				href="https://github.com/jufo-ameisen-2019/ameisen-deep-space-ars-electronica"
+				github-path="jufo-ameisen-2019/ameisen-deep-space-ars-electronica"
 			/>
 		</ProjectsContainer>
 		<ProjectsContainer title="Jugend Forscht 2016">
@@ -88,21 +101,25 @@
 				name="Chickomat"
 				icon="https://raw.githubusercontent.com/chickomat/Chickomat/master/Bilder/icon.png"
 				href="https://github.com/chickomat/Chickomat"
+				github-path="chickomat/Chickomat"
 			/>
 			<Project
 				name="chickomat.github.io"
 				icon="https://raw.githubusercontent.com/chickomat/Chickomat/master/Bilder/icon.png"
 				href="https://chickomat.github.io"
+				github-path="chickomat/chickomat.github.io"
 			/>
 			<Project
 				name="Chickomat Raspberry Software"
 				icon="https://raw.githubusercontent.com/chickomat/Chickomat/master/Bilder/icon.png"
 				href="https://github.com/chickomat/raspberry-control-software"
+				github-path="chickomat/raspberry-control-software"
 			/>
 			<Project
 				name="Chickomat Arduino Software"
 				icon="https://raw.githubusercontent.com/chickomat/Chickomat/master/Bilder/icon.png"
 				href="https://github.com/chickomat/arduino-control-software"
+				github-path="chickomat/arduino-control-software"
 			/>
 		</ProjectsContainer>
 		<ProjectsContainer title="King of Planets">
@@ -111,12 +128,14 @@
 				icon="https://raw.githubusercontent.com/fractava/King-of-Planets/master/King_of_Planets/data/Planet2.png"
 				style="image-rendering: pixelated;"
 				href="https://github.com/fractava/King-of-Planets"
+				github-path="fractava/King-of-Planets"
 			/>
 			<Project
 				name="King of Planets Server"
 				icon="https://raw.githubusercontent.com/fractava/King-of-Planets/master/King_of_Planets/data/Planet2.png"
 				style="image-rendering: pixelated;"
 				href="https://github.com/fractava/King-of-Planets-Server"
+				github-path="fractava/King-of-Planets-Server"
 			/>
 		</ProjectsContainer>
 		<ProjectsContainer title="Home Assistant">
@@ -124,11 +143,13 @@
 				name="Home Assistant Remote CEC"
 				icon="https://www.home-assistant.io/images/home-assistant-logo.svg"
 				href="https://github.com/JonathanTreffler/Home-Assistant-remote-cec"
+				github-path="JonathanTreffler/Home-Assistant-remote-cec"
 			/>
 			<Project
 				name="HACS Websocket Service"
 				icon="https://www.home-assistant.io/images/home-assistant-logo.svg"
 				href="https://github.com/JonathanTreffler/hacs-websocket-service"
+				github-path="JonathanTreffler/hacs-websocket-service"
 			/>
 		</ProjectsContainer>
 		<ProjectsContainer title="Quill Blots">
@@ -136,11 +157,13 @@
 				name="mathQuill Blot"
 				icon="/projects/quill.webp"
 				href="https://github.com/JonathanTreffler/Quill-mathQuill-blot"
+				github-path="JonathanTreffler/Quill-mathQuill-blot"
 			/>
 			<Project
 				name="mathLive Blot"
 				icon="/projects/quill.webp"
 				href="https://github.com/JonathanTreffler/Quill-mathLive-blot"
+				github-path="JonathanTreffler/Quill-mathLive-blot"
 			/>
 		</ProjectsContainer>
 		<ProjectsContainer title="Vue">
@@ -148,11 +171,13 @@
 				name="Vue Gallery"
 				icon="/projects/github.webp"
 				href="https://github.com/JonathanTreffler/modern-vue-gallery"
+				github-path="JonathanTreffler/modern-vue-gallery"
 			/>
 			<Project
 				name="Vuejs Quill"
 				icon="/projects/quill.webp"
 				href="https://github.com/JonathanTreffler/vuejs-quill"
+				github-path="JonathanTreffler/vuejs-quill"
 			/>
 		</ProjectsContainer>
 		<ProjectsContainer title="Miscellaneous">
@@ -162,26 +187,31 @@
 				icon-background="#0082c9"
 				icon-size="90%"
 				href="https://github.com/e-alfred/flowupload"
+				github-path="e-alfred/flowupload"
 			/>
 			<Project
 				name="wann-tritt-scheuer-zurück.de"
 				icon="/projects/scheuer.webp"
 				href="https://github.com/JonathanTreffler/Wann-tritt-Scheuer-zurueck"
+				github-path=""
 			/>
 			<Project
 				name="Kdenlive VNC Docker"
 				icon="/projects/kdenlive.webp"
 				href="https://github.com/JonathanTreffler/kdenlive-vnc-docker"
+				github-path="JonathanTreffler/kdenlive-vnc-docker"
 			/>
 			<Project
 				name="canvas2image 2"
 				icon="/projects/github.webp"
 				href="https://github.com/JonathanTreffler/canvas2image-2"
+				github-path="JonathanTreffler/canvas2image-2"
 			/>
 			<Project
 				name="My Vue Eslintrc"
 				icon="/projects/github.webp"
 				href="https://github.com/JonathanTreffler/my-vue-eslintrc"
+				github-path="JonathanTreffler/my-vue-eslintrc"
 			/>
 		</ProjectsContainer>
 	</div>
